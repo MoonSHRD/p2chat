cca13ddb256e21f3163709e7aa43827939d433c7
Bekket <jackatwork@yandex.ru>
2019-06-24 12:51:26 +0300

    flags

--- mDNS/flags.go
+++ mDNS/flags.go
@@ -14,7 +14,7 @@ type config struct {
 func parseFlags() *config {
 	c := &config{}
 
-	flag.StringVar(&c.RendezvousString, "rendezvous", "meetme", "Unique string to identify group of nodes. Share this with your friends to let them connect with you")
+	flag.StringVar(&c.RendezvousString, "rendezvous", "moonshard", "Unique string to identify group of nodes. Share this with your friends to let them connect with you")
 	flag.StringVar(&c.listenHost, "wrapped_host", "0.0.0.0", "The bootstrap node wrapped_host listen address\n")
 	flag.StringVar(&c.ProtocolID, "pid", "/chat/1.1.0", "Sets a protocol id for stream headers")
 	flag.IntVar(&c.listenPort, "port", 4001, "node listen port")
