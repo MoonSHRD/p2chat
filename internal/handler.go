18dc722cc111c709fd45a375243d7815824e82cb
Bekket <jackatwork@yandex.ru>
2019-06-30 17:19:03 +0300

    fix: handler network request fix
    Former-commit-id: b4e6f82d4c5848702b7e343e62f5bbf0f92be96c

--- internal/handler.go
+++ internal/handler.go
@@ -4,18 +4,21 @@ import (
 	"encoding/json"
 	"fmt"
 	"sync"
-
+	"time"
+	"context"
 
 	"github.com/MoonSHRD/p2chat/api"
 	mapset "github.com/deckarep/golang-set"
 	peer "github.com/libp2p/go-libp2p-peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+
 )
 
 type Handler struct {
 	pb            *pubsub.PubSub
 	serviceTopic  string
 	networkTopics mapset.Set
+	pbMutex				sync.Mutex
 }
 
 func NewHandler(pb *pubsub.PubSub, serviceTopic string, networkTopics *mapset.Set) Handler {
@@ -27,7 +30,7 @@ func NewHandler(pb *pubsub.PubSub, serviceTopic string, networkTopics *mapset.Se
 }
 
 func (h *Handler) HandleIncomingMessage(msg pubsub.Message) {
-	var pbMutex sync.Mutex
+//	var pbMutex sync.Mutex
 	addr, err := peer.IDFromBytes(msg.From)
 	if err != nil {
 		fmt.Println("Error occurred when reading message From field...")
@@ -39,43 +42,73 @@ func (h *Handler) HandleIncomingMessage(msg pubsub.Message) {
 		return
 	}
 	switch message.Flag {
+	// Getting regular message
 	case api.FLAG_GENERIC_MESSAGE:
 		// Green console colour: 	\x1b[32m
 		// Reset console colour: 	\x1b[0m
 		fmt.Printf("%s \x1b[32m%s\x1b[0m> ", addr, message.Body)
+
+	// Getting topic request, answer topic response
 	case api.FLAG_TOPICS_REQUEST:
-		ack := &api.GetTopicsAckMessage{
+		respond := &api.GetTopicsRespondMessage{
 			BaseMessage: api.BaseMessage{
 				Body: "",
 				Flag: api.FLAG_TOPICS_RESPONSE,
 			},
 			Topics: h.getTopics(),
 		}
-		sendData, err := json.Marshal(ack)
+		sendData, err := json.Marshal(respond)
 		if err != nil {
 			return
 		}
 		go func() {
-			pbMutex.Lock()
+			// Lock for blocking "same-time-respond"
+			h.pbMutex.Lock()
 			h.pb.Publish(h.serviceTopic, sendData)
-			pbMutex.Unlock()
+			h.pbMutex.Unlock()
 		}()
+	// Getting topic respond, adding topics to `networkTopics`
 	case api.FLAG_TOPICS_RESPONSE:
-		ack := &api.GetTopicsAckMessage{}
-		err = json.Unmarshal(msg.Data, ack)
+		respond := &api.GetTopicsRespondMessage{}
+		err = json.Unmarshal(msg.Data, respond)
 		if err != nil {
 			return
 		}
-		for i := 0; i < len(ack.Topics); i++ {
-			h.networkTopics.Add(ack.Topics[i])
+		for i := 0; i < len(respond.Topics); i++ {
+			h.networkTopics.Add(respond.Topics[i])
 		}
 	default:
 		fmt.Printf("\nUnknown message type: %#x\n", message.Flag)
 	}
 }
 
-// Get list of topics this node is subscribed to
+// Get list of topics **this** node is subscribed to
 func (h *Handler) getTopics() []string {
 	topics := h.pb.GetTopics()
 	return topics
+}
+
+// Requesting topics from **other** peers
+func (h *Handler) RequestNetworkTopics(ctx context.Context) {
+
+	requestTopicsMessage := &api.BaseMessage{
+		Body: "",
+		Flag: api.FLAG_TOPICS_REQUEST,
+	}
+	sendData, err := json.Marshal(requestTopicsMessage)
+	if err != nil {
+		panic(err)
+	}
+	t := time.NewTicker(3 * time.Second)
+	defer t.Stop()
+	for range t.C {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+		h.pbMutex.Lock()
+		h.pb.Publish(h.serviceTopic, sendData)
+		h.pbMutex.Unlock()
+	}
 }
