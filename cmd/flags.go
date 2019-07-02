fd585fc6144a5ffc1fd98b876fc8feda3273a20d
ChronosX88 <chronosx88@gmail.com>
2019-07-02 11:57:43 +0400

    refactor: Rename protocol ID

--- cmd/flags.go
+++ cmd/flags.go
@@ -16,7 +16,7 @@ func parseFlags() *config {
 
 	flag.StringVar(&c.RendezvousString, "rendezvous", "moonshard", "Unique string to identify group of nodes. Share this with your friends to let them connect with you")
 	flag.StringVar(&c.listenHost, "wrapped_host", "0.0.0.0", "The bootstrap node wrapped_host listen address\n")
-	flag.StringVar(&c.ProtocolID, "pid", "/chat/1.1.1", "Sets a protocol id for stream headers")
+	flag.StringVar(&c.ProtocolID, "pid", "/moonshard/1.0.0", "Sets a protocol id for stream headers")
 	flag.IntVar(&c.listenPort, "port", 4001, "node listen port")
 
 	flag.Parse()
