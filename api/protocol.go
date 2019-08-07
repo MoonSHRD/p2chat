6acc4cde6b25d4249af8e39f3c90e7257abe266a
alyakimenko <urlucidfall@gmail.com>
2019-08-07 15:56:16 +0300

    feat: add peers identity flag constants

--- api/protocol.go
+++ api/protocol.go
@@ -5,11 +5,15 @@ Flags:
 		- 0x0: Generic message
 		- 0x1: Request to get existing PubSub topics at the network
 		- 0x2: Response to the request for topics (ack)
+		- 0x3: Request to ask peers for their MatrixID
+		- 0x4: Response to the request for peers identity
 */
 const (
-	FlagGenericMessage int = 0x0
-	FlagTopicsRequest  int = 0x1
-	FlagTopicsResponse int = 0x2
+	FlagGenericMessage   int = 0x0
+	FlagTopicsRequest    int = 0x1
+	FlagTopicsResponse   int = 0x2
+	FlagIdentityRequest  int = 0x3
+	FlagIdentityResponse int = 0x4
 )
 
 // BaseMessage is the basic message format of our protocol
